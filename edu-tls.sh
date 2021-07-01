4f8df707c3e4e526e0f0f723fd7326ebffaeeb8d
agian123 <85033259+agian123@users.noreply.github.com>
2021-07-01 08:25:19 +0700

    Add files via upload

---
+++ edu-tls.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Proxy For Edukasi & Imclass
+# ==============================
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/edu-tls "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/edu1.py"
+chmod +x /usr/local/bin/edu-tls
+
+# Installing Service
+cat > /etc/systemd/system/edu-tls.service << END
+[Unit]
+Description=Python Edu Tls By Endka
+Documentation=https://sosohoha.my.id
+After=network.target nss-lookup.target
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/edu-tls 2086
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable edu-tls
+systemctl restart edu-tls
+
+clear
