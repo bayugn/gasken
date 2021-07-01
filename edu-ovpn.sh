ea6e7327e984252bb6ad94e38a0d9202003746ed
agian123 <85033259+agian123@users.noreply.github.com>
2021-07-01 08:26:05 +0700

    Update edu-ovpn.sh

--- edu-ovpn.sh
+++ edu-ovpn.sh
@@ -3,7 +3,7 @@
 # ==============================
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/edu-ovpn "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/edu-op.py"
+wget -q -O /usr/local/bin/edu-ovpn "https://raw.githubusercontent.com/agian123/gasken/main/edu-op.py"
 chmod +x /usr/local/bin/edu-ovpn
 
 # Installing Service
